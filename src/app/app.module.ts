4845b9e8d8714e3faccafa4c423538e8d0a7cda8
John Christian Fermin <johnchristian@quantumcrowd.io>
2021-09-15 14:12:04 +0800

    remove user component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { UserComponent } from './components/user/user.component';
 import { UsersComponent } from './components/users/users.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { PostsComponent } from './components/posts/posts.component';
@@ -20,7 +19,6 @@ import { ImageCropperModule } from 'ngx-image-cropper';
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent,
     UsersComponent,
     NavbarComponent,
     PostsComponent,
